d080521a29dd3b22501ffafb069a3da71affc2cb
krishna <krishnasaiyeturu@gmail.com>
2019-10-15 18:26:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+ function add(){
+        var a=parseFloat(document.getElementById("firstnumber").value);
+        var b=parseFloat(document.getElementById("secondnumber").value);
+        document.getElementById("total").value=a+b;
+
+}
